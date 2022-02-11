1dbdd0e5060c4638a07bb9df9870f40909e271d4
Santiago González <48886164+sgtrusty@users.noreply.github.com>
2022-02-10 21:14:11 -0400

    Manually modifying tailwind.config.js
    Fixing classes for 'build' publish (heroku revs)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  purge: [
-	'./pages/**/*.js',
-	'./screens/**/*.js',
-	'./components/**/*.js'
-  ],
+  purge: {
+    content: [
+      './pages/**/*.js',
+      './screens/**/*.js',
+      './components/**/*.js'
+    ],
+    options: {
+      safelist: ['h-10', 'w-10', 'h-48', 'h-full', 'flex', 'justify-center', 'items-center']
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
